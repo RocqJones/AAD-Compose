fbc861ce3164bfbcc0b9f10cdf0d48df645ee3de
RocqJones <jonesmbindyo@gmail.com>
2023-01-25 11:26:26 +0300

    Compose Layouts

--- HeyCompose/app/src/main/java/com/rocqjones/heycompose/MainActivity.kt
+++ HeyCompose/app/src/main/java/com/rocqjones/heycompose/MainActivity.kt
@@ -3,13 +3,20 @@ package com.rocqjones.heycompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.rocqjones.heycompose.model.Message
 import com.rocqjones.heycompose.ui.theme.HeyComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +29,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    MessageCard(Message("Jones Mbindyo", "Hey, take a look at Jetpack Compose, it's great!"))
                 }
             }
         }
@@ -30,14 +37,43 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MessageCard(msg: Message) {
+    // Add padding around our message
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(R.drawable.profile_image),
+            contentDescription = stringResource(R.string.profile_pic),
+            // Set image size to 40 dp and Clip image to be shaped as a circle
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+
+        )
+
+        // Add a horizontal space between the image and the column
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column {
+            Text(text = msg.author)
+
+            // Add a vertical space between the author and message texts
+            Spacer(modifier = Modifier.height(8.dp))
+            
+            Text(text = msg.body)
+        }
+    }
 }
 
+/**
+ * This window shows a preview of the UI elements created by composable functions marked with the
+ * @Preview annotation.
+ */
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     HeyComposeTheme {
-        Greeting("Android")
+        MessageCard(
+            msg = Message("Colleague", "Jetpack compose!")
+        )
     }
 }
---
+++ HeyCompose/app/src/main/java/com/rocqjones/heycompose/model/DataModel.kt
@@ -0,0 +1,3 @@
+package com.rocqjones.heycompose.model
+
+data class Message(val author: String, val body: String)
