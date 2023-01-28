ac005290b8be417a5f340cb312732a5ecaf3dbf4
RocqJones <jonesmbindyo@gmail.com>
2023-01-28 03:06:20 +0300

    Create a list of messages and make message expandable

--- HeyCompose/app/src/main/java/com/rocqjones/heycompose/MainActivity.kt
+++ HeyCompose/app/src/main/java/com/rocqjones/heycompose/MainActivity.kt
@@ -6,12 +6,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
@@ -19,6 +22,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.rocqjones.heycompose.model.Message
+import com.rocqjones.heycompose.model.SampleData
 import com.rocqjones.heycompose.ui.theme.HeyComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,7 +35,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    MessageCard(Message("Jones Mbindyo", "Hey, take a look at Jetpack Compose, it's great!"))
+                    // Single Card
+                    // MessageCard(Message("Jones Mbindyo", "Hey, take a look at Jetpack Compose, it's great!"))
+                    // List or Items
+                    Conversations(messages = SampleData.conversationSample)
                 }
             }
         }
@@ -55,7 +62,15 @@ fun MessageCard(msg: Message) {
         // Add a horizontal space between the image and the column
         Spacer(modifier = Modifier.width(8.dp))
 
-        Column {
+        /**
+         * Animate messages while expanding
+         * - We keep track if the message is expanded or not in this
+         * - variable
+         */
+        var isExpanded by remember { mutableStateOf(false) }
+
+        // We toggle the isExpanded variable when we click on this Column
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
             Text(
                 text = msg.author,
                 color = MaterialTheme.colors.secondaryVariant, // 1. Color
@@ -66,12 +81,41 @@ fun MessageCard(msg: Message) {
             Spacer(modifier = Modifier.height(4.dp))
 
             Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) { // 3. Shape
+//                Text(
+//                    text = msg.body,
+//                    style = MaterialTheme.typography.body2 // Typography
+//                )
                 Text(
                     text = msg.body,
-                    style = MaterialTheme.typography.body2 // Typography
+                    modifier = Modifier.padding(all = 4.dp),
+                    // If the message is expanded, we display all its content
+                    // otherwise we only display the first line
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.body2
                 )
             }
         }
+    }
+}
+
+/**
+ * Lists and animations
+ * - Conversation function that will show multiple messages.
+ */
+@Composable
+fun Conversations(messages : List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(msg = message)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewConversation() {
+    HeyComposeTheme {
+        Conversations(messages = SampleData.conversationSample)
     }
 }
 
