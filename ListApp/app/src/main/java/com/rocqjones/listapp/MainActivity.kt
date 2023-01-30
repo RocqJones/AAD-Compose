4eac7509039c40108281543e51e689c1f6131b9a
RocqJones <jonesmbindyo@gmail.com>
2023-01-30 16:34:38 +0300

    UI update

--- ListApp/app/src/main/java/com/rocqjones/listapp/MainActivity.kt
+++ ListApp/app/src/main/java/com/rocqjones/listapp/MainActivity.kt
@@ -4,8 +4,8 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -15,6 +15,8 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -86,15 +88,6 @@ fun OnBoardingScreen(
     }
 }
 
-// Added a fixed height to verify that the content is aligned correctly.
-@Preview(showBackground = true, widthDp = 320, heightDp = 320)
-@Composable
-fun OnBoardingPreview() {
-    ListAppTheme {
-        OnBoardingScreen(onContinueClicked = {}) // empty lambda expression means "Do nothing" on click
-    }
-}
-
 @Composable
 fun Greetings(
     modifier: Modifier = Modifier,
@@ -122,7 +115,19 @@ fun Greetings(
 }
 
 @Composable
-fun Greeting(name: String) {
+private fun Greeting(name: String) {
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primary
+        ),
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        CardContent(name)
+    }
+}
+
+@Composable
+private fun CardContent(name: String) {
     /**
      * Btn state management with remember.
      * - If you expand item number 1, you scroll away to number 20 and come back to 1, you'll notice that 1 is back to the original size.
@@ -137,39 +142,68 @@ fun Greeting(name: String) {
      * - The spring spec does not take any time-related parameters. Instead it relies on physical
      * properties (damping and stiffness) to make animations more natural.
      */
+    /*
     val extraPadding by animateDpAsState(
         if (expanded.value) 48.dp else 0.dp,
         animationSpec = spring(
             dampingRatio = Spring.DampingRatioMediumBouncy,
             stiffness = Spring.StiffnessLow
         )
-    )
-
-    Surface(
-        color = MaterialTheme.colorScheme.primary,
-        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    )*/
+    Row(
+        modifier = Modifier
+            .padding(12.dp)
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
+            )
     ) {
-        Row(modifier = Modifier.padding(24.dp)) {
-            Column(
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(bottom = extraPadding.coerceAtLeast(0.dp)) // due to animation we're making sure that padding is never negative, otherwise it could crash the app.
-            ) {
-                Text(text = "Hello")
-                Text(text = "$name.",
-                    style = MaterialTheme.typography.headlineMedium.copy(
-                        fontWeight = FontWeight.ExtraBold
-                    )
+        Column(
+            modifier = Modifier.weight(1f).padding(12.dp) //.padding(bottom = extraPadding.coerceAtLeast(0.dp)) // due to animation we're making sure that padding is never negative, otherwise it could crash the app.
+        ) {
+            Text(text = "Hello")
+            Text(text = "$name.",
+                style = MaterialTheme.typography.headlineMedium.copy(
+                    fontWeight = FontWeight.ExtraBold
+                )
+            )
+
+            if (expanded.value) {
+                Text(
+                    text = ("Composem ipsum color sit lazy, padding theme elit, sed do bouncy. ").repeat(4),
                 )
             }
+        }
 
-            ElevatedButton(onClick = { expanded.value = !expanded.value }) {
-                Text(if (expanded.value) "Show less" else "Show more")
-            }
+        /*
+        ElevatedButton(onClick = { expanded.value = !expanded.value }) {
+            Text(
+                if (expanded.value) stringResource(R.string.show_less) else stringResource(R.string.show_more)
+            )
+        }*/
+
+        IconButton(onClick = { expanded.value = !expanded.value }) {
+            Icon(
+                // imageVector = if (expanded) Filled.ExpandLess else Filled.ExpandMore,
+                painter = if (expanded.value) painterResource(id = R.drawable.baseline_expand_less_24) else painterResource(id = R.drawable.baseline_expand_more_24),
+                contentDescription = if (expanded.value) {
+                    stringResource(R.string.show_less)
+                } else {
+                    stringResource(R.string.show_more)
+                }
+            )
         }
     }
 }
 
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "DefaultPreviewDark"
+)
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
@@ -178,12 +212,15 @@ fun DefaultPreview() {
     }
 }
 
-@Preview(
-    showBackground = true,
-    widthDp = 320,
-    uiMode = UI_MODE_NIGHT_YES,
-    name = "Dark"
-)
+// Added a fixed height to verify that the content is aligned correctly.
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnBoardingPreview() {
+    ListAppTheme {
+        OnBoardingScreen(onContinueClicked = {}) // empty lambda expression means "Do nothing" on click
+    }
+}
+
 @Preview
 @Composable
 fun MyAppPreview() {
