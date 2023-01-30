1cbc000948a84a27e4615f21da7e60f124773a2a
RocqJones <jonesmbindyo@gmail.com>
2023-01-30 14:52:15 +0300

    Styling and theming your app

--- ListApp/app/src/main/java/com/rocqjones/listapp/MainActivity.kt
+++ ListApp/app/src/main/java/com/rocqjones/listapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.rocqjones.listapp
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -14,6 +15,7 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.rocqjones.listapp.ui.theme.ListAppTheme
@@ -154,7 +156,11 @@ fun Greeting(name: String) {
                     .padding(bottom = extraPadding.coerceAtLeast(0.dp)) // due to animation we're making sure that padding is never negative, otherwise it could crash the app.
             ) {
                 Text(text = "Hello")
-                Text(text = "$name.")
+                Text(text = "$name.",
+                    style = MaterialTheme.typography.headlineMedium.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    )
+                )
             }
 
             ElevatedButton(onClick = { expanded.value = !expanded.value }) {
@@ -172,6 +178,12 @@ fun DefaultPreview() {
     }
 }
 
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "Dark"
+)
 @Preview
 @Composable
 fun MyAppPreview() {
